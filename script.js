7fa4dede1e641c3b08847b062d189f62b7247849
sanket <95356246+Sankugarud@users.noreply.github.com>
2023-05-31 00:58:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
-//your JS code here. If required.
-let timer = document.getElementById("timer");
+let clock = document.getElementById("timer");
 
-setInterval( () => {
-	  let d = new Date();
-	let day = d.getDate();
-	let month = d.getMonth() + 1;
+setInterval(showTime, 1000);
+function showTime() {
+    let d = new Date();
+	let day = d.getDate()
+	let month = d.getMonth() + 1
 	let year = d.getFullYear();
     let hour = d.getHours();
     let min = d.getMinutes();
     let sec = d.getSeconds();
-    let am_pm = "AM";
-
-	if(hour > 12){
-		hour = hour - 12;
-		am_pm = "PM"
-	}
-	 if (hour == 0) {
-        hour = 12;
+    am_pm = "AM";
+ 
+    if (hour > 12) {
+        hour -= 12;
+        am_pm = "PM";
+    }
+    if (hour == 0) {
+        hr = 12;
         am_pm = "AM";
     }
-
-	if(hour<10){
-		hour = "0" + hour;
-	}
-	if(min<10){
-		min = "0" + min;
-	}
-	if(sec<10){
-		sec = "0" + sec;
-	}
-
-	 let currentTime = `${day}/${month}/${year}, ${hour}:${min}:${sec} ${am_pm}`;
-    timer.innerText = currentTime;
-}, 1000);
+ 
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+ 
+    let currentTime = `${day}/${month}/${year}, ${hour}:${min}:${sec} ${am_pm}`;
+    clock.innerHTML = currentTime;
+}
+showTime();
